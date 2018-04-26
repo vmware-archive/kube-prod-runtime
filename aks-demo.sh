adb20f5847ad153ee42f4dfb69966803436c809c
Angus Lees <gus@inodes.org>
2018-04-26 17:20:10 +1000

    Extend actual demo part of demo script

--- aks-demo.sh
+++ aks-demo.sh
@@ -6,16 +6,38 @@
 #   <git clone repo somewhere>
 #   <ensure azure-cli is installed and setup appropriately: az login>
 #   <setup glue records for azure-controlled DNS zone used in demo.
-#    NS: ns1-09.azure-dns.com. ns2-09.azure-dns.net.
-#        ns3-09.azure-dns.org. ns4-09.azure-dns.info. >
+#    See az network dns zone details for nameservers.>
 #   <optional: set KUBECONFIG to some empty file>
 #   make -C kubeprod
 #   alias kubeprod=$repo/aks-demo.sh
+#   cat <<EOF >wordpress-values.yaml
+#   serviceType: ClusterIP
+#   ingress:
+#     enabled: true
+#     hosts:
+#       - name: wordpress.aztest.oldmacdonald.farm
+#         tls: true
+#         tlsSecret: wordpress-tls
+#         annotations:
+#           kubernetes.io/tls-acme: true
+#   EOF
 # Demo:
 #   az aks create --resource-group prod-runtime-rg --name demo --node-count 3 --node-vm-size Standard_DS2_v2 --generate-ssh-key --kubernetes-version 1.9.4
+#   (creating an AKS cluster typically takes ~15minutes, so don't do that live)
 #   az aks get-credentials --resource-group prod-runtime-rg --name demo --admin --file $KUBECONFIG
 #   kubeprod install --platform aks+k8s-1.9 --email gus@bitnami.com --azure-resource-group prod-runtime-rg --dns-suffix aztest.oldmacdonald.farm
-#   helm install stable/wordpress --set ... (<- TODO)
+#   helm init
+#   helm install stable/wordpress --values wordpress-values.yaml
+#   (wait a few minutes, because nami and letsencrypt/DNS propagation - nami is slower fwiw)
+#   <log in using https URL and user/pass instruction on helm output>
+#   <Note DNS entry exists/works.  Note TLS certificate exists/works.>
+#   Visit https://prometheus.aztest.oldmacdonald.farm/
+#   <Note Azure oauth2 authentication>
+#   Example query: rate(nginx_requests_total{server_zone="wordpress.aztest.oldmacdonald.farm"}[5m])
+#   Visit https://kibana.aztest.oldmacdonald.farm/
+#   See eg: wordpress HTTP request logs
+#   <Note automatic container log collection>
+#
 
 mydir=${0%/*}
 # This can go away once we are a public github project
