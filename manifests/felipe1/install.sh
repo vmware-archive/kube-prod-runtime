71a86b6b0719be4fb0c1853fa89c02e7c8faae8f
Sameer Naik <sameer@bitnami.com>
2018-10-21 15:44:37 +0530

    manifests: remove accidentally committed files
    _read title :slightly_smiling_face_

--- manifests/felipe1/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make -C ../../kubeprod || exit 1
-../../kubeprod/bin/kubeprod install aks --platform=aks+k8s-1.9 --dns-resource-group felipe1 --dns-zone aks.azure.nami.run --email felipe@bitnami.com --manifests ../
-kubectl -n kubeprod get pods --watch
